a71e92e7f6ea0f7f596018e5b9dce337e6152d8f
ivytingli <ivy.ting.li@gmail.com>
2017-10-06 16:18:24 -0400

    Updated hw04 links

---
+++ lib/lib/microblog_web/router.ex
@@ -0,0 +1,34 @@
+defmodule MicroblogWeb.Router do
+  use MicroblogWeb, :router
+  import Microblog.Plugs
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug :fetch_user
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", MicroblogWeb do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+    resources "/posts", PostController
+    resources "/users", UserController
+    resources "/follows", FollowController
+    post "/sessions", SessionController, :login
+    delete "/sessions", SessionController, :logout
+
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", MicroblogWeb do
+  #   pipe_through :api
+  # end
+end
